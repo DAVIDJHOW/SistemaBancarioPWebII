5c6e82c239c2927628850415f49e2bb6fa36ae15
David Augusto <davidaugusto.ifal@gmail.com>
2018-08-30 15:42:19 -0300

    criação da classe pessoa, classe abstrata para implementar a classe funcionario e a classe cliente

---
+++ src/main/java/com/sistemaBancario/sistema/model/Pessoa.java
@@ -0,0 +1,44 @@
+package com.sistemaBancario.sistema.model;
+
+import javax.persistence.Column;
+import javax.persistence.Id;
+
+public abstract class Pessoa  {
+	
+	@Id
+	private Integer id;
+	
+	@Column
+	private String nome;
+	
+	@Column
+	private Endereco endereco;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Endereco getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(Endereco endereco) {
+		this.endereco = endereco;
+	}
+	
+	
+	
+
+}
