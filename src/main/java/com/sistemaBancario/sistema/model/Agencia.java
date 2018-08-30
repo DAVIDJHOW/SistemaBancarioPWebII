3d47ce3acd2c75192b746201fd4adca159f8eb0b
David Augusto <davidaugusto.ifal@gmail.com>
2018-08-30 14:49:07 -0300

    adição dos metodos da classe agencia e dos metodos equals e hashcode

--- src/main/java/com/sistemaBancario/sistema/model/Agencia.java
+++ src/main/java/com/sistemaBancario/sistema/model/Agencia.java
@@ -17,5 +17,69 @@ public class Agencia {
 	
 	@Column(name = "cidade_agencia")
 	private String cidadeAgencia;
+	
+	public Agencia ()
+	{
+		
+	}
+	
+	
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public int getNumeroAgencia() {
+		return numeroAgencia;
+	}
+
+	public void setNumeroAgencia(int numeroAgencia) {
+		this.numeroAgencia = numeroAgencia;
+	}
+
+	public String getCidadeAgencia() {
+		return cidadeAgencia;
+	}
+
+	public void setCidadeAgencia(String cidadeAgencia) {
+		this.cidadeAgencia = cidadeAgencia;
+	}
+
+
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + numeroAgencia;
+		return result;
+	}
+
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Agencia other = (Agencia) obj;
+		if (numeroAgencia != other.numeroAgencia)
+			return false;
+		return true;
+	}
+	
+	
+	
+	
+	
+	
+	
 
 }
