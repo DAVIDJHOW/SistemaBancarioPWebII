d89eed110a897f743713f143b1c47c71e0e14000
Aristoteles <aripbarros@gmail.com>
2018-08-31 10:54:48 -0300

    adição do atributo inicial na classe Cliente e adição de seus metodos

--- src/main/java/com/sistemaBancario/sistema/model/Cliente.java
+++ src/main/java/com/sistemaBancario/sistema/model/Cliente.java
@@ -1,5 +1,46 @@
 package com.sistemaBancario.sistema.model;
 
-public class Cliente {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+
+@Entity
+public class Cliente extends Pessoa {
+	
+	@Column
+	private String RG;
+
+	public String getRG() {
+		return RG;
+	}
+	
+	
+	
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((RG == null) ? 0 : RG.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Cliente other = (Cliente) obj;
+		if (RG == null) {
+			if (other.RG != null)
+				return false;
+		} else if (!RG.equals(other.RG))
+			return false;
+		return true;
+	}
+	
+	
 
 }
