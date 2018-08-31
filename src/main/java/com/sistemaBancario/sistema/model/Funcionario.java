3465f04d19b64321a49e678c4f5dc31f352ed987
Ed <edmilsonoliveira.ifal@gmail.com>
2018-08-31 10:42:56 -0300

    Adição dos atributos e metodos da classe Funcionario

--- src/main/java/com/sistemaBancario/sistema/model/Funcionario.java
+++ src/main/java/com/sistemaBancario/sistema/model/Funcionario.java
@@ -1,9 +1,53 @@
 package com.sistemaBancario.sistema.model;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 
 @Entity
 public class Funcionario extends Pessoa {
+	
+	@Column
+	private int codigo;
+	@Column 
+	private double salario;
+	
+	public int getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+	public double getSalario() {
+		return salario;
+	}
+	public void setSalario(double salario) {
+		this.salario = salario;
+	}
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + codigo;
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Funcionario other = (Funcionario) obj;
+		if (codigo != other.codigo)
+			return false;
+		return true;
+	}
+	
+	
+	
+	
 	
 	
 
