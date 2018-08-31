4fc4d769c9845c694a2c8b1b66087dcf0d14b59e
Aristoteles <aripbarros@gmail.com>
2018-08-31 11:24:12 -0300

    Mapeamento Entity para as classes ContaBancaria e Transacao

--- src/main/java/com/sistemaBancario/sistema/model/Transacao.java
+++ src/main/java/com/sistemaBancario/sistema/model/Transacao.java
@@ -1,9 +1,11 @@
 package com.sistemaBancario.sistema.model;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
+@Entity
 public class Transacao {
 	
 	
