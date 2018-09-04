540937512a02de6ba32bf4d4f63f0fb5777db96f
David Augusto <davidaugusto.ifal@gmail.com>
2018-09-04 00:10:55 -0300

    criação do repositorio de clientes

---
+++ src/main/java/com/sistemaBancario/sistema/repositorios/ClienteRepository.java
@@ -0,0 +1,11 @@
+package com.sistemaBancario.sistema.repositorios;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.sistemaBancario.sistema.model.Cliente;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, Integer>{
+
+}
